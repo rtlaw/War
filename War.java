be29b77616e5343479a6370d9166882fbded13c5
RT <rtlawpl@gmail.com>
2017-06-22 19:43:45 +0200

    DP2

--- War.java
+++ War.java
@@ -2,6 +2,7 @@ package War;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Vector;
 //DP
 public class War
 {
@@ -9,6 +10,8 @@ public class War
 	List<Integer> deck1 = new ArrayList<Integer>();
 	List<Integer> deck2 = new ArrayList<Integer>();
 	List<Integer> table = new ArrayList<Integer>();
+	Vector<Integer> war1 = new Vector<Integer>();
+	Vector<Integer> war2 = new Vector<Integer>();
 	int rounds = 0;
 	int wars = 0;
 	public War()
@@ -27,10 +30,10 @@ public class War
 	{
 	//	int[] tab = {5, 1, 13, 10, 11, 3, 2, 10, 4, 12, 5, 11, 10, 5, 7, 6, 6, 11, 9, 6, 3, 13, 6, 1, 8, 1};
 	//	int[] tab2 = {9, 12, 8, 3, 11, 10, 1, 4, 2, 4, 7, 9, 13, 8, 2, 13, 7, 4, 2, 8, 9, 12, 3, 12, 7, 5};
-	//	int[] tab = {3, 11, 6, 12, 2, 13, 5, 7, 10, 3, 10, 4, 12, 11, 1, 13, 12, 2, 1, 7, 10, 6, 12, 5, 8, 1};
-	//	int[] tab2 = {9, 10, 7, 9, 5, 2, 6, 1, 11, 11, 7, 9, 3, 4, 8, 3, 4, 8, 8, 4, 6, 9, 13, 2, 13, 5};
-		int[] tab = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-		int[] tab2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+		int[] tab = {3, 11, 6, 12, 2, 13, 5, 7, 10, 3, 10, 4, 12, 11, 1, 13, 12, 2, 1, 7, 10, 6, 12, 5, 8, 1};
+		int[] tab2 = {9, 10, 7, 9, 5, 2, 6, 1, 11, 11, 7, 9, 3, 4, 8, 3, 4, 8, 8, 4, 6, 9, 13, 2, 13, 5};
+	//	int[] tab = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+	//	int[] tab2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 		
 		for(int x: tab)
 		{
@@ -73,9 +76,8 @@ public class War
 		}
 		else
 		{
-			if(table.size() != 52)
+			if(Math.max(deck1.size(), deck2.size()) != 0)
 			{
-				
 				if(deck1.size()== 0)
 					playerTwoWon();
 				else if(deck2.size()== 0)
@@ -83,25 +85,12 @@ public class War
 				else
 				{
 					wars++;
-					if(deck1.size()>= 4 && deck2.size() >= 4)
+					int i = 0;
+					while(i != 3 && Math.min(deck1.size(), deck2.size()) != 1)
 					{
-					//	System.out.println("POWPlayer1 = " + deck1.size() + "Player2 = " + deck2.size());
-						table.add(deck1.remove(0));
-						table.add(deck2.remove(0));
-						table.add(deck1.remove(0));
-						table.add(deck2.remove(0));
-						table.add(deck1.remove(0));
-						table.add(deck2.remove(0));
-					}
-					else
-					{
-					//	System.out.println("Player1 = " + deck1.size() + "Player2 = " + deck2.size());
-						while(Math.min(deck1.size(), deck2.size()) != 1)
-						{
-							table.add(deck1.remove(0));
-							table.add(deck2.remove(0));
-						}
-							
+						war1.add(deck1.remove(0));
+						war2.add(deck2.remove(0));
+						i++;
 					}
 					singleRound();
 				}
@@ -110,74 +99,49 @@ public class War
 	}
 	private void playerOneWon()
 	{
-		if(table.size() == 2)
+		if(war1.size() == 0)
 		{
-			deck1.add(table.get(1));
-			deck1.add(table.get(0));
-			cleanTable();
+			deck1.add(table.remove(table.size() - 2));
+			deck1.add(table.remove(table.size() - 1));
+			if(table.size() != 0)
+				playerOneWon();
 		}
 		else
 		{
-			int faceDowns = (((table.size() - 2) % 8) - 2)/2;
-			if(faceDowns < 0)
-				faceDowns = 3;
+			deck1.add(table.remove(table.size() - 2));
+			while(war1.size() != 0)
+				deck1.add(war1.remove(war1.size()-1));
 			
-			System.out.println("Size =" + table.size() + "ile = " + faceDowns);
-			for(int i = 0; i < faceDowns; i++)
-			{
-				deck1.add(table.get(table.size() - (3 + i + faceDowns)));
-			}
-			deck1.add(table.get(table.size() - 2));
-			for(int i = 0; i < faceDowns; i++)
-			{
-				deck1.add(table.get(table.size() - (3 + i)));
-			}
-			deck1.add(table.get(table.size() - 1));
-			
-			for(int i = 0; i < 2+faceDowns*2; i++)
-			{
-				table.remove(table.size() - 1);
-			}
+			deck1.add(table.remove(table.size() - 1));
+			while(war2.size() != 0)
+				deck1.add(war2.remove(war2.size()-1));
+
 			playerOneWon();
 		}
 		
 	}
 	private void playerTwoWon()
 	{
-		if(table.size() == 2)
+		if(war1.size() == 0)
 		{
-			deck2.add(table.get(0));
-			deck2.add(table.get(1));
-			cleanTable();
+			deck2.add(table.remove(table.size() - 1));
+			deck2.add(table.remove(table.size() - 1));
+			if(table.size() != 0)
+				playerTwoWon();
 			
 		}
 		else
 		{
-			int faceDowns = (((table.size() - 2) % 8) - 2)/2;
-			if(faceDowns < 0)
-				faceDowns = 3;
-			System.out.println("Size =" + table.size() + "ile = " + faceDowns);
-			for(int i = 0; i < faceDowns; i++)
-			{
-				deck2.add(table.get(table.size() - (3 + i)));
-			}
-			deck2.add(table.get(table.size() - 1));
-			for(int i = 0; i < faceDowns; i++)
-			{
-				deck2.add(table.get(table.size() - (3 + i + faceDowns)));
-			}
-			deck2.add(table.get(table.size() - 2));
+			deck2.add(table.remove(table.size() - 1));
+			while(war2.size() != 0)
+				deck2.add(war2.remove(war2.size()-1));
 			
 			
-			for(int i = 0; i < 2+faceDowns*2; i++)
-			{
-				table.remove(table.size() - 1);
-			}
-			playerTwoWon();
+			deck2.add(table.remove(table.size() - 2));
+			while(war1.size() != 0)
+				deck2.add(war1.remove(war1.size()-1));
+			playerTwoWon();	
 		}
 	}
-	private void cleanTable()
-	{
-		table.clear();
-	}
+
 }
